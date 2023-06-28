0d0c74f15fee5a9b431e5f1ac5b8aa5246543705
AntonioMCSoyMomo <antoniom@soymomo.com>
2023-06-28 17:25:53 -0400

    Initial Commit

---
+++ MyPokedex/MyPokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPokedexApp.swift
+//  MyPokedex
+//
+//  Created by Antonio Madrid on 28-06-23.
+//
+
+import SwiftUI
+
+@main
+struct MyPokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
